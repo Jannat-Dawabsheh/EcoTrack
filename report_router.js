811812ab548f8cbb8ab3f9b3b3a41139ce0179f4
raghadSabri6 <146375237+raghadSabri6@users.noreply.github.com>
2023-12-25 18:18:19 +0200

    Add routes and handlers for report-related operations.
    Introduce Express routes and corresponding handlers to handle various report-related operations, including reporting, retrieving all reports, and fetching reports based on different criteria such as report ID, user ID, issue, date, and time intervals. The routes are defined in the 'report.js' module.

---
+++ report_router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const {reporting,getAllReports,getReportsByReportID,
+    getReportsByUserID,getReportsByIssue,getReportsBetweenTwoDates,getReportsByDate,
+    getReportsByDateAndBetweenTwoTimes,getReportsBetweenTwoTimes,
+    getReportsBetweenTwoTimesAndDates,getReportsFromStartDateAndTimeToEndDateAndTime,
+    getReportsByTime} = require('./report.js');
+
+
+router.post('/user/reporting', reporting )
+router.get('/user/getAllReports', getAllReports )
+router.get('/user/getReportsByReportID', getReportsByReportID )
+router.get('/user/getReportsByUserID', getReportsByUserID )
+router.get('/user/getReportsByIssue', getReportsByIssue )
+router.get('/user/getReportsBetweenTwoDates', getReportsBetweenTwoDates )
+router.get('/user/getReportsByDate', getReportsByDate )
+router.get('/user/getReportsByDateAndBetweenTwoTimes', getReportsByDateAndBetweenTwoTimes )
+router.get('/user/getReportsBetweenTwoTimes', getReportsBetweenTwoTimes )
+router.get('/user/getReportsBetweenTwoTimesAndDates', getReportsBetweenTwoTimesAndDates )
+router.get('/user/getReportsFromStartDateAndTimeToEndDateAndTime', getReportsFromStartDateAndTimeToEndDateAndTime )
+router.get('/user/getReportsByTime', getReportsByTime )
+
+module.exports = router;
